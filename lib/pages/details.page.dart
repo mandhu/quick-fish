70a732c96dd53b9af09611a0c0cace7a258c2395
Asham Sabir <ashamnx@gmail.com>
2019-07-13 23:00:39 +0500

    changes

--- lib/pages/details.page.dart
+++ lib/pages/details.page.dart
@@ -9,6 +9,7 @@ class ProductDetailsPage extends StatefulWidget {
 class _ProductDetailsPageState extends State<ProductDetailsPage>
     with SingleTickerProviderStateMixin {
   AnimationController _controller;
+  bool isChecked = false;
 
   @override
   void initState() {
@@ -58,34 +59,45 @@ class _ProductDetailsPageState extends State<ProductDetailsPage>
           ),
         ),
         Padding(
-          padding: const EdgeInsets.only(right: 15),
+          padding: const EdgeInsets.only(right: 25, top: 20),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: <Widget>[
               Text(
                 '\$ 100',
-                style: TextStyle(fontSize: 20, color: Color(0xff5FB405)),
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xff5FB405)),
               ),
             ],
           ),
         ),
         Padding(
-          padding: const EdgeInsets.all(15),
+          padding: const EdgeInsets.only(left: 15, right: 15, top: 15),
           child: Row(
             children: <Widget>[
               Expanded(
                 flex: 2,
                 child: Container(
+                  margin: EdgeInsets.only(right: 20),
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      color: Colors.white),
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFFCDDFF2),
+                        blurRadius: 5.0,
+                      )
+                    ],
+                  ),
                   child: TextFormField(
                     style: TextStyle(fontSize: 18),
                     decoration: InputDecoration(
                       hintText: 'Quantity',
                       hintStyle: TextStyle(color: Colors.black26),
                       border: InputBorder.none,
-                      contentPadding: EdgeInsets.all(15),
+                      contentPadding: EdgeInsets.all(10),
                       focusedBorder: InputBorder.none,
                     ),
                   ),
@@ -94,13 +106,36 @@ class _ProductDetailsPageState extends State<ProductDetailsPage>
               MaterialButton(
                 color: Color(0xff3B85D2),
                 textColor: Colors.white,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                child: Text('asdfd'),
+                padding: const EdgeInsets.all(12),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                child: Text('BUY NOW'),
                 onPressed: () {},
               )
             ],
           ),
         ),
+        Row(
+          children: <Widget>[
+            Checkbox(
+              value: isChecked,
+              onChanged: (value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            Text('Delivery?')
+          ],
+        ),
+        
+        TextField(
+          decoration: InputDecoration(
+            labelText: 'Address'
+          ),
+          maxLines: null,
+          keyboardType: TextInputType.multiline,
+        )
       ],
     );
   }
