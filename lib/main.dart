590cba5fb25e73635508b50374436115aef76e7d
Asham Sabir <ashamnx@gmail.com>
2019-07-14 00:17:57 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quick_fish/pages/listings.page.dart';
 import 'package:quick_fish/pages/new-post.page.dart';
+import 'package:quick_fish/pages/profile.page.dart';
 
 import 'pages/details.page.dart';
 
@@ -35,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
       TextStyle(color: Colors.redAccent, fontSize: 12);
   int selectedPage = 0;
 
-  List pages = [NewPostPage(), ListingsPage(), ProductDetailsPage() ];
+  List pages = [NewPostPage(), ListingsPage(), ProfilePage(), ProductDetailsPage() ];
 
   @override
   void initState() {
